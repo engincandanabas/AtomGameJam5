317d0beda6c021fc8829c258b43989463623cb3c
Engincan Danabaş <dnbsengincan@gmail.com>
2022-09-16 13:57:14 +0300

    Heroes

--- Assets/Scripts/Managers/HeroesPlace/HeroesPlaceManager.cs
+++ Assets/Scripts/Managers/HeroesPlace/HeroesPlaceManager.cs
@@ -8,11 +8,13 @@ public class HeroesPlaceManager : MonoBehaviour
 {
     [Header("UI Attributes")]
     [SerializeField] private HorizontalLayoutGroup _heroesPlaceLayout;
-    [SerializeField] private GameObject[] _heroesBoxes;
-    [SerializeField] private TextMeshProUGUI[] _heroesTexts;
+    [SerializeField] private GameObject[] _heroesBoxes; // 0-mouse 1-dog 2-goblin 3-ogre 4-ork 5-gnoll 
+    [SerializeField] private TextMeshProUGUI[] _heroesTexts; // 0-mouse 1-dog 2-goblin 3-ogre 4-ork 5-gnoll 
 
 
     [SerializeField] private int[] _heroesCounts; // 0-mouse 1-dog 2-goblin 3-ogre 4-ork 5-gnoll 
+    
+    private GameObject _preview;
 
     void Awake() {
         InitializeVariables();
@@ -26,7 +28,45 @@ public class HeroesPlaceManager : MonoBehaviour
         if(GameManager.instance.gameState==GameManager.GameState.menu)
         {
             // eger oyun baslamamissa
-            
+            if(_preview!=null)
+            {
+                // Olusturulan heronun mouse'u takip etmesi
+
+                Vector3 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+                _preview.transform.position = new Vector3(mousePos.x,mousePos.y,0);
+
+                // Odaya yerlestirme islemi
+
+                RaycastHit2D hit = Physics2D.Raycast(_preview.transform.position, _preview.transform.forward, 10f);
+                Debug.DrawLine(_preview.transform.position, hit.point);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if(hit.collider!=null)
+                    {
+                        if(hit.collider.CompareTag("Room"))
+                        {
+                            // odaya yerlestirme islemi gercekleþti
+                            Debug.Log("Odaya yerlestirildi");
+                            _preview.transform.parent=hit.transform.GetChild(0);
+                            
+                            // isimiz bitti
+                            _preview = null;
+                            
+                        }
+                    }
+                }
+
+                // Iptal etme islemi
+                if(Input.GetMouseButtonDown(1))
+                {
+                    // iptal olacak
+                    CancelSpawnObjectType(_preview);
+                    Destroy(_preview);
+                    _preview = null;
+                }
+            }
         }
     }
     private void InitializeVariables()
@@ -44,13 +84,18 @@ public class HeroesPlaceManager : MonoBehaviour
         int _activeBoxs = 0;
         for (int i = 0; i < 6; i++)
         {
+            
             _heroesTexts[i].text = "x" + _heroesCounts[i].ToString();
-            if (_heroesCounts[i] == 0)
+            if (_heroesCounts[i] <= 0)
                 _heroesBoxes[i].SetActive(false);
             else
+            {
                 _activeBoxs++;
+                _heroesBoxes[i].SetActive(true);
+            }
 
         }
+        // aktif hero sayisine gore layoutun guncellenmesi
         switch (_activeBoxs)
         {
             case 2:
@@ -72,10 +117,18 @@ public class HeroesPlaceManager : MonoBehaviour
     }
     public void SpawnHero(GameObject heroPrefab)
     {
-        switch(heroPrefab.GetComponent<HeroManager>().type)
+        if(_preview==null)
+        {
+            _preview = Instantiate(heroPrefab);
+            SpawnObjectType(_preview);
+        }
+    }
+    void SpawnObjectType(GameObject gameObject)
+    {
+        switch (gameObject.GetComponent<HeroManager>().type)
         {
             case HeroManager.HeroTYpe.mouse:
-                _heroesCounts[1]--;
+                _heroesCounts[0]--;
                 break;
             case HeroManager.HeroTYpe.dog:
                 _heroesCounts[1]--;
@@ -92,8 +145,36 @@ public class HeroesPlaceManager : MonoBehaviour
             case HeroManager.HeroTYpe.gnoll:
                 _heroesCounts[5]--;
                 break;
+
         }
         // spawn isleminden sonra gereken guncellemenin yapilmasi
         CheckCount();
     }
+    void CancelSpawnObjectType(GameObject gameObject)
+    {
+            switch (gameObject.GetComponent<HeroManager>().type)
+            {
+                case HeroManager.HeroTYpe.mouse:
+                    _heroesCounts[0]++;
+                    break;
+                case HeroManager.HeroTYpe.dog:
+                    _heroesCounts[1]++;
+                    break;
+                case HeroManager.HeroTYpe.goblin:
+                    _heroesCounts[2]++;
+                    break;
+                case HeroManager.HeroTYpe.ogre:
+                    _heroesCounts[3]++;
+                    break;
+                case HeroManager.HeroTYpe.ork:
+                    _heroesCounts[4]++;
+                    break;
+                case HeroManager.HeroTYpe.gnoll:
+                    _heroesCounts[5]++;
+                    break;
+            }
+        // spawn isleminden sonra gereken guncellemenin yapilmasi
+        CheckCount();
+    }
+
 }
